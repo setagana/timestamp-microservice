e45628d0eaf9addb7c2140e6323af32ee785b609
Matthew dB <matthewdb85@gmail.com>
2017-05-10 12:06:06 +0100

    Initial commit of regex patterns for date formats

---
+++ constants/dateFormats.js
@@ -0,0 +1,5 @@
+export const DDMMYYYY_SLASH = '\d{1,2}\/\d{1,2}\/\d{2,4}';
+export const DDmmmYYYY_SPACE = '\d{1,2}([a-zA-Z]{2})?\s[a-zA-Z]{3,9}\s\d{2,4}';
+export const DDMMYYYY_PERIOD = '\d{1,2}\.\d{1,2}\.\d{2}';
+export const YYYYMMDD_HYPHEN = '\d{2,4}-\d{1,2}-\d{1,2}';
+export const DDmmmYYYY_HYPHEN = '\d{1,2}([a-zA-Z]{2})?-[a-zA-Z]{3,9}-\d{2,4}';
--- errors/UnprocessableEntity.js
+++ errors/UnprocessableEntity.js
@@ -1,10 +1,9 @@
-module.exports = function UnprocessableEntity(
-  message = 'The requested string could not be processed - it does not contain a date in a supported format.',
-  errorCode = 422) {
+const defaultMessage = 'The requested string could not be processed - it does not contain a date in a supported format.';
+
+module.exports = function UnprocessableEntity(message = defaultMessage) {
   Error.captureStackTrace(this, this.constructor);
 
   this.name = this.constructor.name;
   this.message = message;
-  this.statusCode = 422;
-  this.errorCode = errorCode;
-}
+  this.status = 422;
+};
